b205282c0d4c31bfbb81eb9a7603d5558415c1ba
Goga Tirkiya <tirkiya@icloud.com>
2019-09-18 23:33:40 +0200

    temp stage

--- src/app/pages/Main/index.tsx
+++ src/app/pages/Main/index.tsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import LogoSvg from './../../assets/img/logo.svg';
 import ArrowSvg from './../../assets/img/arrow.svg';
 import LoginButton from '../../components/LoginButton';
 import MiniQuizView from '../../components/MiniQuizView';
@@ -12,6 +11,7 @@ import LogoutButton from '../../components/LogoutButton';
 import LoginDialog from '../../components/LoginDialog';
 import './style.scss';
 import AdminButton from '../../components/AdminButton';
+import Logo from '../../components/Logo';
 
 interface MainProps {
     fetchMiniQuizzes: any,
@@ -20,11 +20,23 @@ interface MainProps {
     userToken?: UserToken,
 }
 
-class Main extends Component<MainProps> {
+interface MainState {
+    scrollY: number,
+}
+
+class Main extends Component<MainProps, MainState> {
+    constructor(props: MainProps) {
+        super(props);
+
+        this.state = {scrollY: 0};
+    }
+
     componentDidMount() {
         const {userToken} = this.props;
         userToken ? this.props.fetchMiniQuizzesByUser(userToken.user.id)
             : this.props.fetchMiniQuizzes();
+
+        window.addEventListener('scroll', this.onScroll.bind(this));
     }
 
     componentDidUpdate(prevProps: MainProps) {
@@ -33,13 +45,21 @@ class Main extends Component<MainProps> {
         }
     }
 
-    private renderMiniQuizzes(miniQuizzes: MiniQuiz[]) {
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll);
+    }
+
+    onScroll() {
+        this.setState({scrollY: window.scrollY || 0});
+    }
+
+    renderMiniQuizzes(miniQuizzes: MiniQuiz[]) {
         return miniQuizzes.map((q: MiniQuiz) => (
             <MiniQuizView miniQuiz={q} key={q.id} />
         ));
     }
 
-    private updateMiniQuizzes() {
+    updateMiniQuizzes() {
         const {userToken} = this.props;
         userToken ? this.props.fetchMiniQuizzesByUser(userToken.user.id)
             : this.props.fetchMiniQuizzes();
@@ -47,13 +67,23 @@ class Main extends Component<MainProps> {
 
     render() {
         const {userToken, miniQuizzes} = this.props;
+        const {scrollY} = this.state;
+
+        console.log(this.state.scrollY);
+
+        //let height = 100;
+        let headerClassName = 'header background-primary';
+
+        if (scrollY !== 0) {
+            headerClassName += ' collapsed';
+        }
 
         return (
             <div id='main'>
                 {!userToken && <LoginDialog />}
-                <div className='main-background-primary h-100vh'>
+                <div className={headerClassName}>
                     <div className='container-fluid p-5 h-100'>
-                        <div className='row justify-content-end'>
+                        <div className='row panel'>
                             {
                                 userToken &&
                                 userToken.user.role === Role.ADMIN &&
@@ -63,24 +93,27 @@ class Main extends Component<MainProps> {
                             {userToken && <LogoutButton />}
                         </div>
                         <div className='row justify-content-center mt-5'>
-                            <img alt='logo' src={LogoSvg} />
+                            <Logo className='logo' />
                         </div>
-                        <div className='row justify-content-center mt-2'>
-                            <h1 className='text-white'>
+                        <div className='row justify-content-center mt-5'>
+                            <h1 className='text-white welcome-text'>
                                 Welcome to Grid Dynamics
                             </h1>
                         </div>
-                        <div className='row justify-content-center my-5'>
-                            <h1 className='main-title'>
-                                Quiz
-                            </h1>
+                        <div className='row justify-content-center'>
+                            <div>
+                                <h1 className='title'>
+                                    Quiz
+                                </h1>
+                                <p className='subtitle'>by Grid Dynamics</p>
+                            </div>
                         </div>
-                        <div className='row justify-content-center h-50 mt-5'>
+                        <div className='row justify-content-center mt-5 arrow'>
                             <img alt='arrow' src={ArrowSvg} />
                         </div>
                     </div>
                 </div>
-                <div className='main-background-accent h-100vh'>
+                <div className='content'>
                     <div className='d-flex justify-content-center flex-wrap'>
                         {miniQuizzes && this.renderMiniQuizzes(miniQuizzes)}
                     </div>
